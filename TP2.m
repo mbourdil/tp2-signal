14ce962b188d53c68f3071ba407c8c185f696235
mbourdil <mbourdil002@ensc.fr>
2023-11-29 10:30:08 +0100

    fgds

--- TP2.m
+++ TP2.m
@@ -1,3 +1,22 @@
 addpath("Data\Data\")
 %on teste avec un seul sujet
-sujet1 = load("subject_01.mat");
+load('subject_01.mat');
+sujet1 = SIGNAL;
+
+%temps de condition 1
+debut1 = find(SIGNAL(:,18)==1);
+fin1 = debut1+5120;
+temps_condition1 = [debut1 fin1];
+
+%temps de condition 2
+debut2 = find(SIGNAL(:,19)==1);
+fin2 = debut2+5120;
+temps_condition2 = [debut2 fin2];
+
+bloc1_cond1 = temps_condition1(1,:);
+
+figure
+plot(bloc1_cond1(1):bloc1_cond1(2), sujet1(bloc1_cond1(1):bloc1_cond1(2), 2))
+hold on
+plot(bloc1_cond1(1):bloc1_cond1(2), sujet1(bloc1_cond1(1):bloc1_cond1(2), 15))
+legend('FP1','O1')
